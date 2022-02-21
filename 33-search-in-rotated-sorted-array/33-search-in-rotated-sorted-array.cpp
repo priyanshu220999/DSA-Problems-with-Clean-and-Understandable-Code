6887655e9105f2b0d296c3e6c7e92ecd50664d1b
priyanshu220999 <56292213+priyanshu220999@users.noreply.github.com>
2022-02-21 15:23:41 +0530

    Time: 11 ms (15.17%), Space: 11.7 MB (29.77%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,80 +1,49 @@
 class Solution {
     
-    bool is_sorted(vector<int> nums,int left,int right){
-        return nums[left]<=nums[right];
+    public:
+    bool isLeftSorted(vector<int> nums,int l,int mid){
+        return nums[l]<=nums[mid];
     }
     
-    int find_min_index(vector<int> nums,int n,int target){
-        
-        int result = -1;
-        
-        int left = 0,right = n-1;
-        
-        while(left<=right){
-            
-            if(is_sorted(nums,left,right))
-                return left;
-            
-            int mid = (left+right)/2;
-            
-            int next = (mid+1)%n;
-            
-            int prev = (mid-1+n)%n;
-            
-            if(nums[next]>=nums[mid] && nums[prev]>=nums[mid])
-                return mid;
-            else if(is_sorted(nums,left,mid))
-                left = mid+1;
-            else 
-                right = mid-1;
-        }
-        return -1;
+    bool isRightSorted(vector<int> nums,int mid,int r){
+        return nums[mid]<=nums[r];
     }
-    
-    int bin_search(vector<int> nums,int left,int right,int target){
-        
-        while(left<=right){
-            
-            int mid = (left+right)/2;
-            
-            if(nums[mid]==target)
-                return mid;
-            else if(nums[mid]<target)
-                left = mid+1;
-            else
-                right = mid-1;
-        }
-        return -1;
-    }
-    
-public:
     int search(vector<int>& nums, int target) {
         
-        int n = nums.size();
+        int l = 0,h = nums.size()-1;
         
-        if(n==1){
-            if(nums[0]==target)
-                return 0;
-            return -1;
+        while(l<=h){
+            int mid = (l+h)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(isLeftSorted(nums,l,mid)){
+                if(target<nums[mid] && target>=nums[l]){
+                    if(target==nums[l]){
+                        return l;
+                    }
+                    else{
+                        h=mid-1;
+                    }
+                }
+                else{
+                    l = mid+1;
+                }
+            }
+            else {
+                if(target>nums[mid] && target<=nums[h]){
+                    if(target==nums[h]){
+                        return h;
+                    }
+                    else{
+                        l = mid+1;
+                    }
+                }
+                else{
+                    h = mid-1;
+                }
+            }
         }
-        
-        int left = 0,right = n-1;
-        
-        if(is_sorted(nums,left,right))
-            return bin_search(nums,left,right,target);
-        
-        int min_index = find_min_index(nums,n,target);
-        
-        if(nums[min_index]==target)
-            return min_index;
-        
-        if(min_index==n-1)
-            return bin_search(nums,left,right-1,target);
-        
-        if(target<=nums[min_index-1] && target>=nums[left])
-            return bin_search(nums,left,min_index-1,target);
-        
-        return bin_search(nums,min_index+1,right,target);
-        
+        return -1;
     }
 };
