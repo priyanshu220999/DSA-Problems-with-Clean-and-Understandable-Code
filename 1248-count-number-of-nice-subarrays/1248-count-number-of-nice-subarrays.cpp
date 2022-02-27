63ba653fd9e731731ae58cabf5ff1295749fff63
priyanshu220999 <56292213+priyanshu220999@users.noreply.github.com>
2022-02-27 22:51:13 +0530

    Time: 162 ms (74.01%), Space: 73.9 MB (30.89%) - LeetHub

---
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -0,0 +1,63 @@
+class Solution {
+    
+    bool is_odd(int n){
+        return n%2;
+    }
+    
+    int get_odd_count(vector<int> nums){
+        
+        int count = 0;
+        
+        for(int i:nums){
+            if(is_odd(i))
+                count++;
+        }
+        return count;
+    }
+    
+    int atmostK(vector<int> nums,int n,int k){
+        
+        if(k==0)
+            return 0;
+        
+        int i = 0,j = 0,l = 0,count = 0,odd = 0;
+        
+        while(i<n){
+            
+            if(is_odd(nums[i])){
+                
+                odd++;
+                j=i;
+                if(odd<=k)
+                    count +=(j-l+1);
+                else{
+                    while(odd>k){
+                        
+                        if(is_odd(nums[l]))
+                            odd--;
+                        l++;
+                        if(odd<=k)
+                            count+=(j-l+1);
+                    }
+                }
+            }
+            else{
+                if(odd>0 && odd<=k)
+                    count+=(j-l+1);
+            }
+            i++;
+        }
+        return count;
+    }
+    
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        
+        if(n==1)
+            return is_odd(nums[0])?1:0;
+        
+        return atmostK(nums,n,k)-atmostK(nums,n,k-1);
+    }
+};
